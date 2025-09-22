29dae139836e9503490266647b263d97c7935ca0
DanielGui <dandanielofc@gmail.com>
2025-09-22 18:52:35 -0300

    Trocando Home

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="w-full h-screen">
+    <div class="container h-full mx-auto text-center flex justify-center items-center flex-col">
+        <div class="md:max-w-sm space-y-2">
+        <h1 class="font-bold text-2xl">Bem vindo ao HealthPage Web</h1>
+        <p class="text-sm">HealthPage é uma plataforma que visa facilitar o acesso à saúde e ao bem-estar dos usuários.</p>
+        </div>
+    </div>
+</main>
